5824fbd1fa93d61f01e4f7bf897f05d1ac32e0c2
SomeLazyGamer <161995411+SomeLazyGamer@users.noreply.github.com>
2026-05-31 20:21:22 -0400

    Minor logic fixes (again)

--- scripts/init.lua
+++ scripts/init.lua
@@ -16,6 +16,39 @@ Tracker:AddLayouts("layouts/weapons.json")
 -- Locations
 ScriptHost:LoadScript("scripts/locations_import.lua")
 
+print("------------------------DEBUG-----------------------")
+-- Table of All Planets
+Planets = {
+    "Novalis",
+    "Kerwan",
+	"Aridia",
+	"Eudora",
+    "Blarg",
+    "Rilgar",
+    "Umbris",
+    "Batalia",
+    "Orxon",
+	"Gaspar",
+	"Pokitaru",
+    "Hoven",
+    "Gemlik",
+    "Oltanis",
+    "Quartu",
+    "Kalebo III",
+	"Fleet",
+	"Veldin"
+}
+print("Planet list: " .. Planets[1])
+for _, name in pairs(Planets) do
+---@type JsonItem
+---@diagnostic disable-next-line: assign-type-mismatch
+	local infobot = Tracker:FindObjectForCode(name)
+	print("Checking: " .. name)
+	infobot.BadgeText = string.sub(name, 1, 10)
+	infobot:SetOverlayFontSize(12)
+	infobot:SetOverlayAlign("center")
+end
+
 -- AutoTracking for Poptracker
 if PopVersion and PopVersion >= "0.18.0" then
     ScriptHost:LoadScript("scripts/autotracking.lua")
